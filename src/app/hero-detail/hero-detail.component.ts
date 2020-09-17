b964535bc931d3c23efbe31ca089e610d3ee2bc9
Beto-23 <mansilla_alberto@outlook.com>
2020-09-17 02:07:15 -0300

    Server communication

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -23,6 +23,11 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+    .subscribe(() => this.goBack());
+  }
+
   constructor(
     private location: Location,
     private route: ActivatedRoute,
